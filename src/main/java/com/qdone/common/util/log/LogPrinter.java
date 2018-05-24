f6deb7918e9f692569214abdd11ddbfdbce341b5
apple987 <835897006@qq.com>
2018-05-24 11:16:34 +0800

    调整通用日志打印

--- src/main/java/com/qdone/common/util/log/LogPrinter.java
+++ src/main/java/com/qdone/common/util/log/LogPrinter.java
@@ -3,7 +3,6 @@ package com.qdone.common.util.log;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
@@ -31,13 +30,13 @@ import com.qdone.common.util.MacUtils;
 import com.qdone.framework.annotation.Function;
 import com.qdone.framework.core.constant.Constants;
 import com.qdone.framework.core.page.CoreUtil;
-import com.qdone.framework.core.page.MutiSort;
 import com.qdone.framework.exception.RRException;
 
 /**
  * 日志打印
- * 
  * @author 傅为地
+ * 通用日志打印，针对传入多样参数兼容处理
+ *  如果传入参数为空，或者只有response。日志不会写入数据库
  */
 @Component
 @Aspect
@@ -45,7 +44,7 @@ public class LogPrinter {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	private String userName = null; // 用户名
-	private Object inputParamMap = new Object(); // 传入参数
+	private String inputArgString="";//传入参数格式化字符串
 	private String className = null;// 执行类名
 	private String methodName = null;// 执行方法名称
 	private String functionName = "";// function注解描述的方法名称
@@ -71,7 +70,7 @@ public class LogPrinter {
 	LogUtil logUtil;
 	
 	/* 切入日志打印 */
-	@Pointcut("execution(public * com.qdone.module.controller..*.*(..)) && @annotation(com.qdone.framework.annotation.Function)")
+	@Pointcut("execution(public * com.qdone.module..*.*(..)) && @annotation(com.qdone.framework.annotation.Function)")
 	public void printLog() {
 	}
 
@@ -93,21 +92,14 @@ public class LogPrinter {
 				Object[] args = pjp.getArgs();
 				Method method = ((org.aspectj.lang.reflect.MethodSignature) pjp.getSignature()).getMethod();
 				Class<?>[] paremClassTypes = method.getParameterTypes();
-				//自身类.class.isAssignableFrom(自身类或子类.class)  返回true 
-				for (int i = 0; i < args.length; i++) {
-					if (Map.class.isAssignableFrom(paremClassTypes[i])) {// 打印map
-						inputParamMap = args[i];
-					}
-					if (MutiSort.class.isAssignableFrom(paremClassTypes[i])) {//MutiSort本类或者子类
-						inputParamMap = args[i];
-					}
-				}
+				inputArgString=CoreUtil.buildInputParamToString(args,paremClassTypes);
+			    //获取注解信息
 				Function f = method.getAnnotation(Function.class);
 				functionName = f == null ? "" : f.value();
 				/*线程池绑定log打印时间*/
 				long beginTime = System.currentTimeMillis();//1、开始时间  
 			    startTimeThreadLocal.set(beginTime);		//线程绑定变量（该数据只有当前请求的线程可见）
-				logger.info(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,S").format(beginTime)+" 用户:" + userName + " [" + functionName + "] 开始:" + className + "." + methodName + "() 参数:"+ JSON.toJSONStringWithDateFormat(inputParamMap, "yyyy-MM-dd HH:mm:ss"));
+				logger.info(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,S").format(beginTime)+" 用户:" + userName + " [" + functionName + "] 开始:" + className + "." + methodName + "() 参数:"+ inputArgString);
 				// 执行完方法的返回值：调用proceed()方法，就会触发切入点方法执行
 				Object result=null;
 				try {
@@ -125,15 +117,7 @@ public class LogPrinter {
 						    sysLog.setRemoteIp(request.getRemoteHost());
 						    sysLog.setRequestUri(request.getRequestURI());//访问路径
 						    sysLog.setRequestMethod(request.getMethod());//请求方式
-						    String actionAccessContent = "";
-							if(inputParamMap!=null &&!inputParamMap.equals("")){
-								if(inputParamMap instanceof String){
-									actionAccessContent=CoreUtil.getNotNullStr(inputParamMap);
-								}else{
-									actionAccessContent=JSON.toJSONStringWithDateFormat(inputParamMap, "yyyy-MM-dd HH:mm:ss");
-								}
-							}
-						    sysLog.setRequestParams(actionAccessContent);
+						    sysLog.setRequestParams(inputArgString);
 						    sysLog.setRequestMac(MacUtils.getMac());
 						    sysLog.setException(null);
 						    sysLog.setClassName(className);
@@ -147,11 +131,9 @@ public class LogPrinter {
 								logger.info(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,S").format(endTime)+" 用户:" + userName + " [" + functionName + "] 结束:" + className + "." + methodName + "() 返回:{}");
 							} else {
 								if (result instanceof String) {
-									logger.info(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,S").format(endTime)+" 用户:" + userName + " [" + functionName + "] 结束:" + className + "." + methodName + "() 返回:"
-											+ result);
+									logger.info(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,S").format(endTime)+" 用户:" + userName + " [" + functionName + "] 结束:" + className + "." + methodName + "() 返回:"+ result);
 								} else {
 									logger.info(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,S").format(endTime)+" 用户:" + userName + " [" + functionName + "] 结束:" + className + "." + methodName + "() 返回:"+ JSON.toJSONStringWithDateFormat(result, "yyyy-MM-dd HH:mm:ss"));
-											
 								}
 							}
 							sysLog.setActionEndTime(new Date(endTime));
@@ -230,4 +212,5 @@ public class LogPrinter {
 				}
 			}
 	}
+  
 }
